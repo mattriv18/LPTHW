b1a5242b82ad2c9fb426a6f25b0e5c8cdd6aa650
mattriv18 <mattriv18@gmail.com>
2017-06-13 23:45:44 -0400

    Create ex1.py

--- LPTHW/ex1.py
+++ LPTHW/ex1.py
@@ -1,8 +1,5 @@
 print "Hello World!"
 print "Hello World Again"
-# dank memes
 print "I like typing like this"
 print "This is fun"
 print 'Yay! Printing!'
-print "I'd much rather 'not'."
-print 'I "said" do not touch this'
